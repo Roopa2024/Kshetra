64cd47ebaf699461dfc50ca1e199f3306c81d558
Roopa2024 <160216705+Roopa2024@users.noreply.github.com>
2025-05-13 10:56:38 +0530

    Update pdf_data.py
    Added support for Debit voucher
    Move dimension values to the ini file

--- pdf_data.py
+++ pdf_data.py
@@ -1,7 +1,8 @@
+import tkinter as tk
 import os, configparser, io, qrcode, sys
 import pandas as pd
 import shutil
-import canvas_update, excel_data
+import canvas_update, canvas_update_voucher, excel_data
 import tkinter.messagebox as messagebox
 from PyPDF2 import PdfReader, PdfWriter
 from reportlab.lib.utils import ImageReader
@@ -24,7 +25,42 @@ copy_font_name = config.get('FontSettings', 'copy_font_name')
 font_size = config.getint('FontSettings', 'font_size')
 x_position = config.getint('FontSettings', 'y_position')
 y_position = config.getint('FontSettings', 'y_position')
- 
+printer_name = config.get('printer', 'printer')
+
+x_top_contributor_name = config.getint('top_dimensions', 'x_contributor_name')
+y_top_contributor_name = config.get('top_dimensions', 'y_contributor_name')
+x_bottom_contributor_name = config.getint('bottom_dimensions', 'x_contributor_name')
+y_bottom_contributor_name = config.get('bottom_dimensions', 'y_contributor_name')
+
+x_top_address = config.getint('top_dimensions', 'x_address')
+y_top_address = config.get('top_dimensions', 'y_address')
+x_bottom_address = config.getint('bottom_dimensions', 'x_address')
+y_bottom_address = config.get('bottom_dimensions', 'y_address')
+
+x_top_pan = config.getint('top_dimensions', 'x_pan')
+y_top_pan = config.get('top_dimensions', 'y_pan')
+x_bottom_pan = config.getint('bottom_dimensions', 'x_pan')
+y_bottom_pan = config.get('bottom_dimensions', 'y_pan')
+x_bottom_pan_HP = config.getint('HP_dimensions', 'x_pan')
+y_bottom_pan_HP = config.get('HP_dimensions', 'y_pan')
+
+x_top_contributor_type = config.getint('top_dimensions', 'x_contributor_type')
+y_top_contributor_type = config.get('top_dimensions', 'y_contributor_type')
+x_bottom_contributor_type = config.getint('bottom_dimensions', 'x_contributor_type')
+y_bottom_contributor_type = config.get('bottom_dimensions', 'y_contributor_type')
+x_top_contributor_intent = config.getint('top_dimensions', 'x_contributor_intent')
+y_top_contributor_intent = config.get('top_dimensions', 'y_contributor_intent')
+x_bottom_contributor_intent = config.getint('bottom_dimensions', 'x_contributor_intent')
+y_bottom_contributor_intent = config.get('bottom_dimensions', 'y_contributor_intent')
+
+x_bank_name = config.getint('top_dimensions', 'x_bank_name')
+y_bank_name = config.get('top_dimensions', 'y_bank_name')
+x_branch_name = config.getint('top_dimensions', 'x_branch_name')
+y_branch_name = config.get('top_dimensions', 'y_branch_name')
+
+y_top_print_date = config.get('top_dimensions', 'y_print_date')
+y_bottom_print_date = config.getint('bottom_dimensions', 'y_print_date')
+
 # Function to get the pdf directory path for EXE
 def get_pdf_directory():
     if hasattr(sys, '_MEIPASS'):    # Running as a bundled .exe
@@ -72,43 +108,50 @@ def generate_duplicates(pdf_path, entity):
     shutil.copy(pdf_path, duplicate_pdf_path_acct)
 
 # Function to generate pdf content based on the inputs given in the APP UI
-def create_pdf_from_kwargs(kwargs, pdf_path, entity, with_bg, top_text, bottom_text):
+def create_pdf_from_kwargs(kwargs, pdf_path, entity, with_bg, top_text, bottom_text, pdf):
     packet = io.BytesIO()
     c = canvas.Canvas(packet, pagesize=A4)
     width, height = A4
     height = height - 56
-    x_offset = 90                                               # Set starting position for text
+    
+    #x_offset = 90                                               # Set starting position for text
     receipt = int(entity in ('SGPM_DN.pdf', 'SPK_DPS.pdf'))
-
+    
     c.setFont(font_name, font_size)     
     for key, value in kwargs.items():
         if key not in ["Barcode", "QR Code"] and value:         # Exclude image fields
             match key:
                 case 'Receipt Date':
-                    canvas_update.draw_receipt_date(c, value, receipt)
+                    canvas_update.draw_receipt_date(c, value, receipt, pdf)
                 case 'Contributor Name':
-                    if receipt: c.drawString(x_offset, height - 105, f"{value}")          
-                    c.drawString(x_offset, height - 495, f"{value}")       
+                    if receipt: c.drawString(x_top_contributor_name, eval(y_top_contributor_name), f"{value}")          
+                    c.drawString(x_bottom_contributor_name, eval(y_bottom_contributor_name), f"{value}")       
                 case 'Address':
-                    if receipt: canvas_update.wrap_text(c, f"{value}", x_offset-50, height - 115, 500, font_name, font_size, receipt)
-                    canvas_update.wrap_text(c, f"{value}", x_offset-50, height - 505, 500, font_name, font_size, 1)
+                    if receipt: canvas_update.wrap_text(c, f"{value}", x_top_address, eval(y_top_address), 500, font_name, font_size, receipt)
+                    canvas_update.wrap_text(c, f"{value}", x_bottom_address, eval(y_bottom_address), 500, font_name, font_size, 1)
                 case 'PAN':
                     c.setFont(font_name, font_size) 
-                    if receipt: c.drawString(402, height - 148, "    ".join(str(value).upper()))
-                    c.drawString(402, height - 538, "    ".join(str(value).upper()))
+                    if pdf:
+                        x = x_bottom_pan
+                        y = eval(y_bottom_pan)
+                    else:
+                        x = x_bottom_pan_HP
+                        y = eval(y_bottom_pan_HP)
+                    if receipt: c.drawString(x_top_pan, eval(y_top_pan), "    ".join(str(value).upper()))
+                    c.drawString(x, y, "    ".join(str(value).upper()))
                     #c.setFont(font_name, font_size) 
                 case 'Contributor Type':
-                    if receipt: c.drawString(x_offset + 70 , height - 170, f"{value}")
-                    c.drawString(x_offset + 70, height - 560, f"{value}")
+                    if receipt: c.drawString(x_top_contributor_type , eval(y_top_contributor_type), f"{value}")
+                    c.drawString(x_bottom_contributor_type, eval(y_bottom_contributor_type), f"{value}")
                 case 'Contributor Intent':
-                    if receipt: c.drawString(x_offset + 70, height - 187, f"{value}")
-                    c.drawString(x_offset + 70, height - 575, f"{value}")
+                    if receipt: c.drawString(x_top_contributor_intent, eval(y_top_contributor_intent), f"{value}")
+                    c.drawString(x_bottom_contributor_intent, eval(y_bottom_contributor_intent), f"{value}")
                 case 'Bank Date':
                     canvas_update.draw_bank_date(c, value, receipt)
                 case 'UTR No.':
-                    canvas_update.draw_utrn(c, value, receipt)
+                    canvas_update.draw_utrn(c, value, receipt, pdf)
                 case 'Amount':
-                    canvas_update.draw_amount(c, value, receipt)
+                    canvas_update.draw_amount(c, value, receipt, pdf)
                 case 'Payment Mode':
                     canvas_update.draw_payment_mode_tick(c, value, receipt)
                 case 'Cheque Date':
@@ -121,15 +164,15 @@ def create_pdf_from_kwargs(kwargs, pdf_path, entity, with_bg, top_text, bottom_t
                     canvas_update.draw_acct_no(c, value, receipt)
                 case 'Bank Name':  
                     c.setFont(font_name, font_size)
-                    if not receipt: c.drawString(x_offset-4, height - 215, f"{value}")
+                    if not receipt: c.drawString(x_bank_name, eval(y_bank_name), f"{value}")
                 case 'Branch Name':  
-                    if not receipt: c.drawString(x_offset + 175, height - 215, f"{value}")
+                    if not receipt: c.drawString(x_branch_name, eval(y_branch_name), f"{value}")
                 case 'Print Date':
                     c.setFont(copy_font_name, 8)  # Set font and size
                     c.setFillColorRGB(0.5, 0.5, 0.5) 
                     text_width = c.stringWidth(value, copy_font_name, 8)
-                    canvas_update.draw_copy_type(c, (height - 305) - text_width, value)
-                    canvas_update.draw_copy_type(c, 15, value)
+                    canvas_update.draw_copy_type(c, eval(y_top_print_date) - text_width, value)
+                    canvas_update.draw_copy_type(c, y_bottom_print_date, value)
                     c.setFont(font_name, font_size)
                     #c.drawString(475, 5, f"{value}")
 
@@ -177,19 +220,24 @@ def create_pdf_from_kwargs(kwargs, pdf_path, entity, with_bg, top_text, bottom_t
                 writer = PdfWriter()
                 for page in new_pdf.pages:
                     writer.add_page(page)
+                
+                if pdf:
+                    pdf_path = os.path.join("Accountant_copy", entity_name, pdf_name)
+
                 with open(str(pdf_path), "wb") as f:                     # Save the final filled PDF
                     writer.write(f)
-                absolute_path = os.path.abspath(pdf_path)
-                print(f"Attempting to open: {absolute_path}")
+                
+                if not pdf:
+                    absolute_path = os.path.abspath(pdf_path)
+                    print(f"Attempting to open: {absolute_path}")
 
-                if os.path.exists(absolute_path):
-                    os.startfile(absolute_path)
-                else:
-                    print("File not found on disk!")
+                    if os.path.exists(absolute_path):
+                        os.startfile(absolute_path)
+                    else:
+                        print("File not found on disk!")
                 duplicate_pdf_path_acct = os.path.join("Accountant_copy", entity_name, pdf_name)
                 merge_pdf(entity, new_pdf, duplicate_pdf_path_acct)
-                #os.startfile(pdf_path)
-        elif with_bg == 1:                                               # print without background
+        elif with_bg == 1:                                               # print with background
             if top_text != bottom_text:
                 merge_pdf(entity, new_pdf, pdf_path)
                 generate_duplicates(pdf_path, entity)
@@ -200,4 +248,58 @@ def create_pdf_from_kwargs(kwargs, pdf_path, entity, with_bg, top_text, bottom_t
                     os.startfile(absolute_path)
                 else:
                     print("File not found on disk!")
+    print(f"PDF saved: {pdf_path}")
+
+def create_pdf_from_kwargs_voucher(dv_entries, pdf_path, entity, with_bg, dest_excel_path):
+    packet = io.BytesIO()
+    c = canvas.Canvas(packet, pagesize=A4)
+    width, height = A4
+    height = height - 56
+    #x_offset = 90   
+
+    c.setFont(font_name, font_size)  
+    for i, receipt in enumerate(dv_entries):
+        receipt_date = receipt['date'].get()
+        pay_to = receipt['pay_to'].get()
+        amount = receipt['amount'].get()
+        purpose = receipt['purpose'].get("1.0", tk.END).strip()
+        canvas_update_voucher.draw_receipt_date(c, i, receipt_date)
+        canvas_update_voucher.draw_voucher(c, i, amount, pay_to, purpose, dest_excel_path, pdf_path)
+
+    # Save PDF
+    c.showPage()
+    c.save()
+
+    packet.seek(0)
+    if packet.getbuffer().nbytes > 0:
+        new_pdf = PdfReader(packet)
+    else:
+        print("The file is empty. Please check the PDF generation process.")
+
+    if not entity == 0:     
+        pdf_name = os.path.basename(pdf_path)
+        entity_name = os.path.splitext(entity)[0]  
+        if with_bg == 0:                                            #print without bg
+            writer = PdfWriter()
+            for page in new_pdf.pages:
+                writer.add_page(page)
+            with open(str(pdf_path), "wb") as f:                     # Save the final filled PDF
+                writer.write(f)
+            absolute_path = os.path.abspath(pdf_path)
+            print(f"Attempting to open: {absolute_path}")
+
+            if os.path.exists(absolute_path):
+                os.startfile(absolute_path)
+            else:
+                print("File not found on disk!")
+        elif with_bg == 1:                                               # print without background
+            merge_pdf(entity, new_pdf, pdf_path)
+            generate_duplicates(pdf_path, entity)
+            absolute_path = os.path.abspath(pdf_path)
+            print(f"Attempting to open: {absolute_path}")
+
+            if os.path.exists(absolute_path):
+                os.startfile(absolute_path)
+            else:
+                print("File not found on disk!")
     print(f"PDF saved: {pdf_path}")
