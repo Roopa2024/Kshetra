f1c9d82651ba6713b1c688bd7309de1e4a4feca8
Roopa2024 <160216705+Roopa2024@users.noreply.github.com>
2025-06-30 13:32:45 +0530

    Delete backend directory

--- backend/top_widget_handling.py
+++
@@ -1,7 +0,0 @@
-import configparser
-import os, sys
-
-configuration_path = os.path.dirname(os.path.abspath(__file__))+"\config\config.ini"
-config = configparser.ConfigParser()
-config.read(configuration_path)
-bank_IFSC = {key: config.get('Bank', key, fallback="HDFC_IFSC") for key in ['HDFC_IFSC', 'KAR_IFSC', 'CNR_IFSC','UBI_IFSC']}
